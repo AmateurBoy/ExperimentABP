5b2c23ec65b767440bcc7968121281faba868fd4
Илья <marchenko.ilya99@gmail.com>
2023-05-18 09:35:20 +0200

    Добавьте файлы проекта.

---
+++ Controllers/ExperimentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AB_testsABP.Controllers
+{
+    [ApiController]
+    [Route("experiment")]
+    public class ExperimentController : Controller
+    {
+        [HttpGet("button_color")]
+        public IActionResult GetExpirementButtonColor([FromQuery(Name = "device-token")] string deviceToken)
+        {
+            return Json("Hello");
+        }
+        [HttpGet("price")]
+        public IActionResult GetExpirementPrice([FromQuery(Name = "device-token")] string deviceToken)
+        {
+            return Json("Hello");
+        }
+    }
+}
